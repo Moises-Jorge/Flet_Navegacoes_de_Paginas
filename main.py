f0631b78b6cff032aa30dc3baf06835f3794311a
Moisés Jorge <moisesjorge30@gmail.com>
2025-04-03 14:37:22 +0100

    criacao do modulo principal e configuracao inicial da pagina

---
+++ main.py
@@ -0,0 +1,27 @@
+import flet as ft
+
+
+def main(page: ft.Page) -> None:
+    # Configuracoes da pagina
+    page.bgcolor = '#8ab9eb'
+    page.theme_mode = ft.ThemeMode.DARK
+    page.title = 'Navegacoes'
+    page.window.width = 450
+    page.window.height = 700
+    page.window.maximizable = False
+    
+    
+    # Criacao da Stack Principal
+    
+    
+    # Criacao das funcoes
+    
+    
+    # Criacao dos widgets
+    
+    
+    # Adicao dos widgets na pagina
+    page.add()
+
+
+ft.app(target=main)
