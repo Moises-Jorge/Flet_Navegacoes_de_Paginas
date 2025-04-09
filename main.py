1539c3b3de66ef1fe014bcd118a3dcc1311b11fe
Moisés Jorge <moisesjorge30@gmail.com>
2025-04-09 10:51:32 +0100

    criacao da barra de navegacao inferior e do botao CENTRAL

--- main.py
+++ main.py
@@ -6,9 +6,29 @@ def main(page: ft.Page) -> None:
     page.bgcolor = '#8ab9eb'
     page.theme_mode = ft.ThemeMode.DARK
     page.title = 'Navegacoes'
-    page.window.width = 450
+    page.window.width = 380
     page.window.height = 700
     page.window.maximizable = False
+    
+    
+    # Criacao do botao central
+    page.floating_action_button = ft.FloatingActionButton(icon=ft.Icons.ADD, bgcolor='blue')
+    page.floating_action_button_location = ft.FloatingActionButtonLocation.CENTER_DOCKED
+    
+    # Criacao da barra de navegacao inferior
+    page.bottom_appbar = ft.BottomAppBar(
+        bgcolor='#f6f6f6ff',
+        shape=ft.NotchShape.CIRCULAR,
+        content=ft.Row(
+            controls=[
+                ft.IconButton(icon=ft.Icons.EDIT, icon_color=ft.Colors.BLUE, icon_size=26),
+                ft.IconButton(icon=ft.Icons.SEARCH, icon_color=ft.Colors.BLUE, icon_size=26),
+                ft.Container(expand=True),
+                ft.IconButton(icon=ft.Icons.SETTINGS, icon_color=ft.Colors.BLUE, icon_size=26),
+                ft.IconButton(icon=ft.Icons.SHARE, icon_color=ft.Colors.BLUE, icon_size=26)
+            ]
+        )
+    )
     
     
     # Criacao da Stack Principal
